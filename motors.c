fcc5f36fa95dcc2b8beb9b59ba5cad0857703f8e
NextGen Code Innovations <thellamaminecart.com@outlook.com>
2024-02-21 11:28:23 -0500

    saved

--- motors.c
+++ motors.c
@@ -18,14 +18,26 @@ void calibrate(){
 //==================================================================================================
 // move forward function
 rotation_reset_position(drivetrainSensor);
-    rotation_reset(drivetrainSensor);
-    while(rotation_get_position(drivetrainSensor) < [motor movemnt in centi-degrees]*[amount of rotation]){
-        motor_move(FRONT_LEFT_MOTOR, 100);
-        motor_move(FRONT_RIGHT_MOTOR, 100);
-        motor_move(BACK_RIGHT_MOTOR, 100);
-        motor_move(BACK_LEFT_MOTOR, 100);
-    }
-    motor_brake(FRONT_LEFT_MOTOR);
-    motor_brake(FRONT_RIGHT_MOTOR);
-    motor_brake(BACK_RIGHT_MOTOR);
-    motor_brake(BACK_LEFT_MOTOR);
+rotation_reset(drivetrainSensor);
+while(rotation_get_position(drivetrainSensor) < [motor movemnt in centi-degrees]*[amount of rotation]){
+    motor_move(FRONT_LEFT_MOTOR, 100);
+    motor_move(FRONT_RIGHT_MOTOR, 100);
+    motor_move(BACK_RIGHT_MOTOR, 100);
+    motor_move(BACK_LEFT_MOTOR, 100);
+}
+motor_brake(FRONT_LEFT_MOTOR);
+motor_brake(FRONT_RIGHT_MOTOR);
+motor_brake(BACK_RIGHT_MOTOR);
+motor_brake(BACK_LEFT_MOTOR);
+
+//=====================================================================================================
+//turn function
+motor_move_relative(BACK_RIGHT_MOTOR, tick*90, -127);
+motor_move_relative(BACK_LEFT_MOTOR, tick*90, 127);
+motor_move_relative(FRONT_LEFT_MOTOR, tick*90, 127);
+motor_move_relative(FRONT_RIGHT_MOTOR, tick*90, -127);
+delay(5000);
+motor_brake(BACK_RIGHT_MOTOR);
+motor_brake(BACK_LEFT_MOTOR);
+motor_brake(FRONT_RIGHT_MOTOR);
+motor_brake(FRONT_LEFT_MOTOR);
